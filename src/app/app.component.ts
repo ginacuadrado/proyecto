d2507fb1e0bc08d1cd6b4344023d02d1cc975ef5
Samuel Velasquez <velasquez.samuel@correo.unimet.edu.ve>
2018-10-08 19:11:05 -0400

    prueba

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'STEAK';
+  catch= 'Lo único imposible es aquello que no pruebas';
+}
---
+++ src/app/components/menu/aperitivos/aperitivos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aperitivos',
+  templateUrl: './aperitivos.component.html',
+  styleUrls: ['./aperitivos.component.scss']
+})
+export class AperitivosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
