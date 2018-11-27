10b61214239b44460fe1ad0e097bd2bc3add9836
Samuel Velasquez <velasquez.samuel@correo.unimet.edu.ve>
2018-11-27 04:19:31 -0400

    fixed commit

--- src/app/components/ventanapago/ventanapago.component.ts
+++ src/app/components/ventanapago/ventanapago.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderServiceService } from '../header/header-service.service';
+import { Item } from '../../models/item';
 
 @Component({
   selector: 'app-ventanapago',
@@ -8,10 +9,48 @@ import { HeaderServiceService } from '../header/header-service.service';
 })
 export class VentanapagoComponent implements OnInit {
 
+  pago: string = "alt";
+  carrito: Item[] = [];
+  subtotal: number = 0;
+  iva: number = 0;
+  envio: number = 0;
+  total: number = 0;
+  i: number = 0;
+
   constructor(private nav: HeaderServiceService) { }
 
   ngOnInit() {
     this.nav.show();
+
+    if(sessionStorage["carritoItems"]){
+      this.carrito = JSON.parse(sessionStorage.getItem('carritoItems'));
+
+    //Suma de los precios
+      for(this.i = 0; this.i<this.carrito.length; this.i++)
+      {
+        this.subtotal = this.subtotal + this.carrito[this.i].precio;
+      }
+
+      this.iva = this.subtotal * 0.16;
+      this.envio = this.subtotal * 0.04;
+      this.total = this.subtotal + this.iva + this.envio;
+    }
+  }
+
+  paypalSelected(event){
+    this.pago = "paypal";
+  }
+
+  altSelected(event){
+    this.pago = "alt";
+  }
+
+  returnSelection(event){
+    this.pago = "select";
+  }
+
+  SubmitAlt(){
+    
   }
 
 }
